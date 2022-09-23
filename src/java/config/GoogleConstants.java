a6812291d2ced66b0923e7787bd3e6ccaaedce56
thn126 <thiennhse162041@fpt.edu.vn>
2022-09-23 09:45:20 +0700

    Khung

---
+++ src/java/config/GoogleConstants.java
@@ -0,0 +1,22 @@
+package config;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Thien's
+ */
+public class GoogleConstants {
+
+    public static String GOOGLE_CLIENT_ID = "779040387699-c58vkqmlf6cmvtv3som469pl5k78lgar.apps.googleusercontent.com";
+    public static String GOOGLE_CLIENT_SECRET = "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";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8084/recruitment-system/GoogleSignInServlet";
+    public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}
