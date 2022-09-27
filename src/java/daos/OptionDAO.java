a1b7cec93ac58cb8892781a56b2a682c153f5d3c
Kallamore <hoangthse162042@fpt.edu.vn>
2022-09-27 15:11:13 +0700

    Update OptionDAO.java

--- src/java/daos/OptionDAO.java
+++ src/java/daos/OptionDAO.java
@@ -7,6 +7,7 @@ package daos;
 
 import dtos.OptionDTO;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -19,7 +20,8 @@ import utils.DBUtils;
  * @author ACER
  */
 public class OptionDAO {
-    public List<OptionDTO> listAll() throws SQLException, ClassNotFoundException{
+
+    public List<OptionDTO> listAll() throws SQLException, ClassNotFoundException {
         List<OptionDTO> list = null;
         Connection con = DBUtils.makeConnection();
         Statement stm = con.createStatement();
@@ -29,6 +31,25 @@ public class OptionDAO {
             OptionDTO opt = new OptionDTO();
             opt.setOp_id(rs.getInt("op_id"));
             opt.setQ_id(rs.getString("q_id"));
+            opt.setContent(rs.getString("content"));
+            opt.setIsCorrect(rs.getBoolean("isCorrect"));
+            list.add(opt);
+        }
+        con.close();
+        return list;
+    }
+
+    public List<OptionDTO> listOneQuestion(String id) throws SQLException, ClassNotFoundException {
+        List<OptionDTO> list = null;
+        Connection con = DBUtils.makeConnection();
+        PreparedStatement stm = con.prepareStatement("select * from [Options] where [q_id] = ?");
+        stm.setString(1, id);
+        ResultSet rs = stm.executeQuery();
+        list = new ArrayList();
+        while (rs.next()) {
+            OptionDTO opt = new OptionDTO();
+            opt.setOp_id(rs.getInt("op_id"));
+            opt.setQ_id(id);
             opt.setContent(rs.getString("content"));
             opt.setIsCorrect(rs.getBoolean("isCorrect"));
             list.add(opt);
