8919b740945d441562f491c9d23a07fea2eb801c
hoangth263 <hoangthse162042@fpt.edu.vn>
2022-11-03 22:04:38 +0700

    Q&A

---
+++ src/java/controllers/QAController.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import daos.MajorDAO;
+import daos.NotificationDAO;
+import dtos.GoogleDTO;
+import dtos.MajorDTO;
+import dtos.NotificationDTO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author ACER
+ */
+@WebServlet(name = "QAController", urlPatterns = {"/Q&A"})
+public class QAController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            HttpSession session = request.getSession();
+            GoogleDTO google = (GoogleDTO) session.getAttribute("info");
+            if (google != null) {
+                NotificationDAO nDao = new NotificationDAO();
+                List<NotificationDTO> notify = nDao.select(google.getEmail());
+                request.setAttribute("listNotification", notify);
+                request.setAttribute("count", nDao.count(google.getEmail()));
+            }
+            MajorDAO majorDao = new MajorDAO();
+            List<MajorDTO> listMajor;
+            listMajor = majorDao.listAll();
+            request.setAttribute("listMajor", listMajor);
+            String action = request.getParameter("op");
+            request.setAttribute("controller", "Q&A");
+            request.setAttribute("action", action);
+            switch (action){
+                case "view":
+                    break;
+                case "create":
+                    break;
+                case "createHandler":
+                    break;
+                case "answer":
+                    break;
+                case "report":
+                    break;
+                case "reportHandler":
+                    break;
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(QAController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    protected void view(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
